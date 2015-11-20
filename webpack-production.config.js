aadb26ca7702b02263599b75cacd6e5ffd2f536e
MiladNazeri <milad@miladnazeri.com>
2015-11-20 02:46:36 -0500

    heroku another try with public path

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/public'
+    publicPath: path.join(__dirname, 'dist/public/')
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
