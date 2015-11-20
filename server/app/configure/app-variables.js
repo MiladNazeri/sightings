68ec01f14517f56659ebaa188bb1e638902d060a
MiladNazeri <milad@miladnazeri.com>
2015-11-20 03:04:57 -0500

    HEROKUgit add .

--- server/app/configure/app-variables.js
+++ server/app/configure/app-variables.js
@@ -5,7 +5,7 @@ var util = require('util');
 
 var rootPath = path.join(__dirname, '../../../');
 var indexPath = path.join(rootPath, './dist/public/index.html');
-var faviconPath = path.join(rootPath, './dist/public/favicon.ico');
+var faviconPath = path.join(rootPath, '../../dist/public/favicon.ico');
 
 var env = require(path.join(rootPath, './server/env'));
 
