7ffaa2c947ed16c071c728ce9b05607a2fa4a96d
MiladNazeri <milad@miladnazeri.com>
2015-11-20 02:39:21 -0500

    again heroku

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/public'
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
