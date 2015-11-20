71b87fdbe932681a78e7f9632d678aa5d64d3bd6
MiladNazeri <milad@miladnazeri.com>
2015-11-20 04:26:18 -0500

    man

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-console.log("IM BEING TOUCHED MAIN.JS")
+
 
 ReactDOM.render( <App />, document.getElementById('main') );
