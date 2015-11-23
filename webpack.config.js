f650c93a76584ca74317ab5519b33e037ed5231b
Amit <amit@prehype.com>
2015-11-23 14:10:34 -0500

    got map to work ended up using no external library

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
      entry: "./src/js/main",
@@ -14,9 +15,23 @@ module.exports = {
       loaders: [{
           test: /\.(js|jsx|babel)$/,
           exclude: /(node_modules|bower_components)/,
-          loaders: ['react-hot', 'babel'],
+          // saving the "include" properties in case we need them later 
+          //include: "/Users/Amit/Dropbox/Coding/FullStack/sightings/node_modules/react-googlemaps/src/ui/components/*.js",
+          // include: [
+          //   path.resolve(__dirname, 'server'),
+          //   path.resolve(__dirname, 'src'),
+          //   path.resolve(__dirname, 'tests'),
+          //   path.resolve(__dirname, 'node_modules/react-googlemaps')
+          // ],
+          loaders: ['react-hot', 'babel', 'babel-loader'],
         }]
-      }
+      },
+      // saving this just in case we need it for some reason
+      // resolveLoader: {
+      //   moduleDirectories: [
+      //     '/Users/Amit/dropbox/coding/fullstack/sightings/node_modules'
+      //   ]
+      // }
     //  module: {
     //       loaders: [
     //            {
