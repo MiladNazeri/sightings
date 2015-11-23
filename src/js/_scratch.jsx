9589a5814da788cbb91537ed49b9eded02e2d170
MiladNazeri <milad@miladnazeri.com>
2015-11-23 13:54:03 -0500

    commits

---
+++ src/js/_scratch.jsx
@@ -0,0 +1,9 @@
+//login Component
+    -render input for un and password and calls AuthService
+export default class Login extends React.Component {
+    constructor() {
+    this.state = {
+
+    }
+    }
+}
